b7d3dd8618d5d669cd08e9c38672b4c6ebe2325e
Simeon Vincent <svincent@gmail.com>
2016-11-13 21:49:57 -0800

    Demo weird  reference for nested WCs

---
+++ wc/currentDoc/parent-async.js
@@ -0,0 +1,33 @@
+/* jshint esversion: 6 */
+(function() {
+  'use strict';
+
+  class ChildImportAsync extends HTMLElement {
+    constructor() {
+      super();
+
+      this._shadowRoot = this.attachShadow({mode: 'closed'});
+
+      // Programmatically style the shadow root
+      const style = document.createElement('style');
+      style.innerText = ':host { display: block; background: LightGoldenrodYellow; }';
+      this._shadowRoot.appendChild(style);
+
+      // Expose the BaseURI for debugging purposes
+      const p = document.createElement('p');
+      p.innerHTML = `Parent Async BaseURI: ${document.currentScript.baseURI}`;
+      this._shadowRoot.appendChild(p);
+
+      const doc = document.currentScript.ownerDocument;
+      const template = doc.getElementById('parent-async');
+      if (template) {
+        const instance = template.content.cloneNode(true);
+        this._shadowRoot.insertBefore(instance, p);
+      } else {
+        console.error('Could not find "parent-async"');
+      }
+    }
+  }
+
+  window.customElements.define('parent-async', ChildImportAsync);
+})();
